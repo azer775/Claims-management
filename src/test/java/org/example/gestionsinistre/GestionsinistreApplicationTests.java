0daee8f63a6cfe68f9532f155d8ce8064f64ba4a
azer775 <80197483+azer775@users.noreply.github.com>
2024-11-06 02:23:47 +0100

    jpa3

--- src/test/java/org/example/gestionsinistre/GestionsinistreApplicationTests.java
+++ src/test/java/org/example/gestionsinistre/GestionsinistreApplicationTests.java
@@ -3,7 +3,11 @@ package org.example.gestionsinistre;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties={
+        "spring.datasource.url=jdbc:mysql://localhost:3306/StageDB",
+        "spring.datasource.username=root",
+        "spring.datasource.password="
+})
 class GestionsinistreApplicationTests {
 
     @Test
