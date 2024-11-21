11c61912b91d5eaaa7d841e6245a52d012d84bdf
azer775 <80197483+azer775@users.noreply.github.com>
2024-11-21 04:50:15 +0100

    Update AuthentificationController.java

--- src/main/java/org/example/gestionsinistre/controllers/AuthentificationController.java
+++ src/main/java/org/example/gestionsinistre/controllers/AuthentificationController.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-//@CrossOrigin("*")
+@CrossOrigin("*")
 @RequestMapping("auth")
 public class AuthentificationController {
     @Autowired
